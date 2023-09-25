6d0177e6db7080817c1d522474a5f2c30b75b048
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-09-25 14:20:07 -0500

    Experimentos en casa

--- sketch.js
+++ sketch.js
@@ -2,6 +2,19 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+
+
 function draw() {
-  circle(mouseX, mouseY, 20);
+  background(255);
+  fill(mouseX);
+  strokeWeight(mouseY);
+  stroke(5,10,5,50);
+  circle(windowWidth / 2,windowHeight / 2, 200 );
+  textSize( 20);
+  text(mouseX, 50, 50);
+  text(int (mouseY) , 50, 100);
+  
+  fill(100, 149, 237, 127);
+  rect(400,400,100,100);
+  line(600,600,750,750);
 }
