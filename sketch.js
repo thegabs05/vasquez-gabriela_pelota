1bc9a272ce623c76671375b3206d191d20c8454a
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-09-25 14:51:26 -0500

    pelota inicio

--- sketch.js
+++ sketch.js
@@ -2,19 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-
-
 function draw() {
-  background(255);
-  fill(mouseX);
-  strokeWeight(mouseY);
-  stroke(5,10,5,50);
-  circle(windowWidth / 2,windowHeight / 2, 200 );
-  textSize( 20);
-  text(mouseX, 50, 50);
-  text(int (mouseY) , 50, 100);
   
-  fill(100, 149, 237, 127);
-  rect(400,400,100,100);
-  line(600,600,750,750);
-}
+  circle(mouseX , mouseY, 20 );
+  }
