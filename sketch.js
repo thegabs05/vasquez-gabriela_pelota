4767cce9f68c4c2f821535bf3174046892e93e23
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-10-20 19:47:11 -0500

    pelota final

--- sketch.js
+++ sketch.js
@@ -6,21 +6,7 @@ let colores;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // print(posX);
-  // posX = 234;
-  // print(posX)
-  // posX = posX + 25;
-  // print(posX)
-  // posX = posX * 2;
-  // print(posX)
-  // posX = posX /5;
-  // print(posX)
-  // posX *= 8 ;
-  // print(posX)
-  // posX += 1 ;
-  // print(posX)
-  // posX ++;
-  // print(posX)
+
   posX = windowWidth / 2;
   posY = windowHeight / 2;
   //frameRate(10);
